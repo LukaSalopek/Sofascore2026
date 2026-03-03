dbe49aa82432801902267af17854cb77950d3b48
Luka Salopek <luka.salopek47@gmail.com>
2026-03-03 15:44:54 +0100

    Dodao SnapKit

---
+++ Zadatak1/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Zadatak1",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Zadatak1",
+            targets: ["Zadatak1"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Zadatak1"
+        ),
+        .testTarget(
+            name: "Zadatak1Tests",
+            dependencies: ["Zadatak1"]
+        ),
+    ]
+)
