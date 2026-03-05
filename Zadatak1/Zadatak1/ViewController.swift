40ef7d43b38c892d62f3cc7388a03e7024e045bc
Luka Salopek <luka.salopek47@gmail.com>
2026-03-05 15:20:12 +0100

    Dodao SnapKit preko SPM

---
+++ Zadatak1/Zadatak1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zadatak1
+//
+//  Created by akademija on 05.03.2026..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
