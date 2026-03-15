6329cbd96c690ac9d8bf9e8e55805815169229f8
Luka Salopek <luka.salopek47@gmail.com>
2026-03-15 20:52:10 +0100

    Dovrsio do kraja

--- Zadatak2/Zadatak2/ViewControllers/ViewController.swift
+++ Zadatak2/Zadatak2/ViewControllers/ViewController.swift
@@ -1,70 +1,89 @@
-//
-//  ViewController.swift
-//  Zadatak2
-//
-//  Created by akademija on 10.03.2026..
-//
-
 import UIKit
 import SofaAcademic
 import SnapKit
 
-
 class ViewController: UIViewController {
     
-    private var leagueView=LeagueView()
-    let data = Homework2DataSource()
-    
+    private var leagueView = LeagueView()
+    private let data = Homework2DataSource()
     lazy var matches = data.laLigaEvents()
     lazy var league = data.laLigaLeague()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLeague()
+        configureLeague()
         setupMatchViews()
-        
     }
     
-    
-    func setupLeague(){
+    func setupLeague() {
         view.addSubview(leagueView)
-        leagueView.snp.makeConstraints{
+        leagueView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             $0.left.right.equalToSuperview()
+            $0.height.equalTo(56)
         }
-        var leagueName=downloadImage(from: league.logoUrl!)
-        leagueView.configure(leagueLogo: leagueName, countryName: league.country!.name, leagueName: league.name)
     }
     
-    func setupMatchViews(){
-        
-        var matchView=MatchView()
-        matchView.snp.makeConstraints{
-            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            $0.left.right.equalToSuperview().inset(20)
-        }
-        
+    func configureLeague() {
+        leagueView.configure(
+            leagueLogo: league.name,
+            countryName: league.country?.name ?? "",
+            leagueName: league.name
+        )
     }
     
-        func downloadImage(from urlString: String) async -> UIImage? {
-            guard let url = URL(string: urlString) else { return nil }
+    func setupMatchViews() {
+        var previousView: UIView = leagueView
+        matches.sort{ $0.startTimestamp < $1.startTimestamp}
+        matches.forEach { match in
+            let matchView = MatchView()
+            view.addSubview(matchView)
             
-            do {
-                let (data, _) = try await URLSession.shared.data(from: url)
-                return UIImage(data: data)
-            } catch {
-                print("Greška pri preuzimanju: \(error)")
-                return nil
+            matchView.updateHomeLogo(firstLogo: match.homeTeam.name)
+            matchView.updateAwayLogo(secondLogo: match.awayTeam.name)
+            
+            let date = Date(timeIntervalSince1970: TimeInterval(match.startTimestamp))
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm"
+            let time = formatter.string(from: date)
+            matchView.setMatch(firstName: match.homeTeam.name, secondName: match.awayTeam.name, matchTime: time)
+            
+            switch match.status {
+            case .notStarted:
+                matchView.updateTime(time: AppStrings.notStarted)
+            case .inProgress:
+                matchView.updateScore(firstScore: match.homeScore!, secondScore: match.awayScore!)
+                matchView.isLive()
+                let diff = Int(Date().timeIntervalSince(date) / 60)
+                matchView.updateTime(time: "\(diff)'")
+            case .halftime:
+                matchView.updateScore(firstScore: match.homeScore!, secondScore: match.awayScore!)
+                matchView.isLive()
+                matchView.updateTime(time: AppStrings.halftime)
+            case .finished:
+                matchView.updateScore(firstScore: match.homeScore!, secondScore: match.awayScore!)
+                if match.homeScore! > match.awayScore! {
+                    matchView.firstWinner()
+                } else if match.homeScore! < match.awayScore! {
+                    matchView.secondWinner()
+                } else {
+                    matchView.draw()
+                }
+                matchView.updateTime(time: AppStrings.finished)
             }
+            
+            matchView.snp.makeConstraints {
+                $0.leading.equalToSuperview()
+                $0.left.right.equalToSuperview().inset(10)
+                $0.top.equalTo(previousView.snp.bottom).offset(10)
+                $0.height.equalTo(35)
+            }
+            previousView = matchView
         }
-    
-
-
+    }
 }
 
-
-
-#Preview{
+#Preview {
     ViewController()
 }
-
